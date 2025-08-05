0c05055ca3af072b148fa72014c874d8c2653ca5
Vignesh Raj D <105130180+vigneshraj-d@users.noreply.github.com>
2025-08-05 11:49:54 +0530

    Create main.tf
    VPC Modules

---
+++ modules/vpc/main.tf
@@ -0,0 +1,45 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr_block
+  tags = merge(var.tags, { Name = "vpc-main" })
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(var.tags, { Name = "public-subnet-${count.index}" })
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = merge(var.tags, { Name = "private-subnet-${count.index}" })
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags = merge(var.tags, { Name = "igw" })
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = merge(var.tags, { Name = "public-route-table" })
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
